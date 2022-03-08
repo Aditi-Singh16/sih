25e96faae96b2f2c4c20f9779cbe89aa851a4831
ryan10projects <59692861+ryan10projects@users.noreply.github.com>
2022-03-08 11:54:56 +0530

    Update ticket_details.dart
    few changes

--- lib/ui/operator/ticket_details.dart
+++ lib/ui/operator/ticket_details.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -6,10 +7,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 String monument_name="";
 final List<String> imgList = [
   '',
-  '',
-  '',
-  '',
-  '',
+
 ];
 void main() async  {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,11 +30,8 @@ class _ticksState extends State<ticks> {
         Map<String, dynamic> data = docSnapshot.data()!;
         setState(() {
           monument_name = data['monumentName'];
-          imgList[0]=data['gallery'][0];
-          imgList[1]=data['gallery'][1];
-          imgList[2]=data['gallery'][2];
-          imgList[3]=data['gallery'][3];
-          imgList[4]=data['gallery'][4];
+          imgList[0]=data['mainPic'];
+
 
         });
         // You can then retrieve the value from the Map like this:
@@ -65,110 +60,121 @@ class _ticksState extends State<ticks> {
               ),
             ),
             backgroundColor: Colors.white,
-            body:Column(
+            body:Container(
+              decoration: BoxDecoration(
+                color:const Color(0xffC8E3EF),
 
-              children: [
-               SizedBox(
-                  height:  height*0.04,
-                ),
-                Expanded(
-                  child: Center(
-                    child: Image.network(imgList[0],height:height*1,width:width*1),
-
-
-                  ),
-                ),
-
-
+              ),
+              child:
                 Column(
+
                   children: [
-                    ConstrainedBox(
-                      constraints: BoxConstraints(maxWidth: 250),
-                      child: Text(
-                        monument_name,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-
-                            fontWeight: FontWeight.bold, fontSize: fontSize, height: 1.2),
-                      ),
+                   SizedBox(
+                      height:  height*0.04,
                     ),
-                 SizedBox(
-                      height:  height*0.02,
-                    ),
+                    Column(
+
+                      children: [
+                        SizedBox(
+                          height:  height*0.04,
+                        ),
+                        //space
+                        Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            child: Image.network(imgList[0],
+                              width:width*10,
+                              height:height*0.27,)  ),
+
+                        SizedBox(
+                          height:  height*0.006,
+                        ),
+                        //monument name
+                        Text(
+                          monument_name,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,fontSize: fontSize),
+                        ),
+                     SizedBox(
+                          height:  height*0.01,
+                        ),
 
 
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
 
-                    ),
+                        ),
 
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                    ),
-                    Container(
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        ),
+                        Container(
 
-                      child: Column(
-                        children: [
+                          child: Column(
+                            children: [
 
-                          Container(
+                              Container(
 
 
-                            child: Column(
-                              children: [
-                                Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
+                                child: Column(
                                   children: [
+                                    Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "Time " + "|",
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle( fontSize: fontSize),
+                                        ),
+                                        Text(
+                                          "Date ",
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle( fontSize: fontSize),
+                                        ),
+                                      ],
+                                    ),
                                     Text(
-                                      "Time" + "|",
+                                      "Location ",
                                       textAlign: TextAlign.center,
                                       style: TextStyle( fontSize: fontSize),
                                     ),
-                                    Text(
-                                      "Date ",
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle( fontSize: fontSize),
+                                     SizedBox(
+                                      height:   height*0.01,
                                     ),
+                                    Padding(
+                                        padding: const EdgeInsets.symmetric(horizontal: 30),
+                                        child: Image.network("https://www.investopedia.com/thmb/ZG1jKEKttKbiHi0EkM8yJCJp6TU=/1148x1148/filters:no_upscale():max_bytes(150000):strip_icc()/qr-code-bc94057f452f4806af70fd34540f72ad.png",
+                                          width:width*10,
+                                          height:height*0.27,)  ),
                                   ],
                                 ),
-                                Text(
-                                  "Location ",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle( fontSize: fontSize),
+                              ),
+                               SizedBox(
+                                height: height*0.02,
+                              ),
+                              OutlineButton(
+                                child: new Text("Cancel Ticket"),
+                                borderSide: BorderSide(
+                                  color: Colors.black,
                                 ),
-                                 SizedBox(
-                                  height:   height*0.02,
-                                ),
-                                Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 30),
-                                    child: Image.network("https://www.investopedia.com/thmb/ZG1jKEKttKbiHi0EkM8yJCJp6TU=/1148x1148/filters:no_upscale():max_bytes(150000):strip_icc()/qr-code-bc94057f452f4806af70fd34540f72ad.png",
-                                      width:width*10,
-                                      height:height*0.27,)  ),
-                              ],
-                            ),
-                          ),
-                           SizedBox(
-                            height: height*0.02,
-                          ),
-                          OutlineButton(
-                            child: new Text("Cancel Ticket"),
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                            ),
-                            onPressed: () {
-                              Navigator.push(context, new MaterialPageRoute(
-                                  builder: (context) => new Monu())
-                              );
-                            },
-                          ),
+                                onPressed: () {
+                                  Navigator.push(context, new MaterialPageRoute(
+                                      builder: (context) => new Monu())
+                                  );
+                                },
+                              ),
 
-                        ],
+                            ],
 
-                      ),
-                    ),
+                          ),
+                        ),
+
+                      ],
+                    )
 
                   ],
-                )
-              ],
+                ),
+
             )
 
         )
